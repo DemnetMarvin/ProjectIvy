cce014c3cd96abdf99abb9c6c45995eef6b6c905
Marvin DEMNET <demnet.marvin@gmail.com>
2017-04-12 19:05:05 +0200

    OpenCSV

--- src/example/Hello.java
+++ src/example/Hello.java
@@ -1,6 +1,10 @@
 
 package example;
 
+import java.io.FileReader;
+import com.opencsv.*;
+import java.io.IOException;
+import java.util.*;
 import org.apache.commons.lang.WordUtils;
 
 public class Hello {
@@ -9,6 +13,20 @@ public class Hello {
         System.out.println("standard message : " + message);
         System.out.println("capitalized by " + WordUtils.class.getName() 
                                 + " : " + WordUtils.capitalizeFully(message));
+		  	try{
+				  	CSVReader reader = new CSVReader(new FileReader("data.csv"));
+				  	List<String[]> myEntries = reader.readAll();
+				  	int monmax = 0;
+				  	int myval=0;
+				  	for(String tab[]: myEntries){
+				  		for(String val: tab){
+				  			myval=Integer.parseInt(val);
+				  			System.out.println(myval);
+							if(myval>monmax) monmax=myval;
+				  		}
+				  	}
+				  	System.out.println("MON MAX : "+monmax);
+				  }catch(IOException e){System.out.println("Fichier non trouvé");}
     }
 }
         
